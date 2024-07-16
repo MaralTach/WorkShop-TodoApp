1d03293c23709b3ab403eed4c6a02ffa3635e754
pmammedov <pmammedov@gmail.com>
2024-07-16 19:50:31 +0200

    chore: Add error handling middleware and database connection configuration

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+
+//* Required modules
+require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+require('express-async-errors')
+
+//* Configurations
+require('./src/configs/dbConnection')()
+
+//* Middlewares
+app.use(express.json())
+
+
+app.all('/', (req, res) => {
+    res.send({
+        error: false,
+        message: 'Welcome to the backend of the application'
+    })
+})
+
+
+
+app.use(require('./src/middlewares/errorHandler'))
+
+app.listen(PORT, () => console.log('listening at http://127.0.0.1:' + PORT))
---
+++ backend/src/middlewares/errorHandler.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = (err, req, res, next) => {
+    const statusCode = res.statusCode ?? 500
+    return res.status(statusCode).send({
+        error: true,
+        message: err.message,
+        cause: err.cause,
+    })
+}
