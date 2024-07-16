cac9b647085058180fee640f95f120f32f995db5
pmammedov <pmammedov@gmail.com>
2024-07-16 20:17:27 +0200

    feat: Add Todo model with title, description, priority, and isDone fields

---
+++ backend/src/models/todo.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { Schema, model } = require('mongoose')
+
+
+const todoSchema = new Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 3,
+        maxlength: 200
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 3,
+        maxlength: 200
+    },
+    priorty: {
+        type: String,
+        enum: {
+            values: ['low', 'medium', 'high'],
+            message: 'Priority must be low, medium or high!'
+        },
+        default: 'medium'
+    },
+    isDone: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    collection: 'todos',
+    timestamps: true
+})
+
+module.exports = model('Todo', todoSchema)
